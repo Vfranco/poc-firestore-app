2ed71f3cf522585f436f354573287a6649b27613
victorfranco25 <110496606+victorfranco25@users.noreply.github.com>
2023-03-01 15:16:13 -0500

    Initial Commit

---
+++ poc-firestore-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poc-firestore-app
+//
+//  Created by BCP on 1/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
